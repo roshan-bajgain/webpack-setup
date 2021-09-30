35258ff27a0ae080e9bed4133926af3949777545
Roshan Bajgain <you@example.com>
2021-09-30 20:35:07 +0545

    Store data into local storage

--- src/function.js
+++ src/function.js
@@ -1,15 +1,15 @@
 export const setCompleted = (todoList, box) => {
-    // const id = box.dataset.id;
-    // const checked = box.checked;
-    // console.log('id: ', id, 'checked: ', checked);
+   const id = box.dataset.id;
+        const checked = box.checked;
+     // console.log('id: ', id, 'checked: ', checked);
 
-    // const element = todoList.find( item => item.index.toString() === id);
-    // element.completed = checked;
+       const element = todoList.find( item => item.index.toString() === id);
+      element.completed = checked;
 
-    saveData(todoList);
-    // item.completed = !(item.complted);
-    // return item;
-}
+      saveData(todoList);
+      item.completed = !(item.complted);
+      return item;
+     }
 
 export const saveData = (list) => {
     list.sort((a, b) => a.index - b.index);
