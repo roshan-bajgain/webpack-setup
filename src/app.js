de5a460bb5988acabf66c3614217d969161576c9
Roshan Bajgain <you@example.com>
2021-10-09 00:30:12 +0545

    Implement reviewer's suggestions

--- src/app.js
+++ src/app.js
@@ -53,6 +53,9 @@ const task = document.querySelectorAll('.task');
 function deleteTask(idx) {
   const storage = JSON.parse(localStorage.getItem('todos'));
   const savedata = storage.filter((value, index) => index !== idx);
+  savedata.forEach((obj, index) => {
+    obj.index = index + 1;
+  });  
   localStorage.setItem('todos', JSON.stringify(savedata));
   window.location.reload();
 }
