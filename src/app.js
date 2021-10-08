1bd0c377704ef2e39fcbc17c553382e18018e186
Roshan Bajgain <you@example.com>
2021-10-08 22:44:29 +0545

    fix linter

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 import './style.css';
-import { setCompleted } from './function.js';
-import { inputBox, showTask, createlocalstorage, addBtn, listArr } from './script.js';
-import { each } from 'lodash';
+import {
+  createlocalstorage, addBtn, listArr,
+} from './script.js';
 
-  const navtoggle = document.querySelector('#ellipse');
-  const container = document.querySelector('.list-container');
-  const deleteallbtn = document.querySelector('.clear');
-  
-  const display = (array) => {
-    let containerItems = '';
-    array.forEach((item) => {
-       containerItems += `
+const container = document.querySelector('.list-container');
+const deleteallbtn = document.querySelector('.clear');
+
+const display = (array) => {
+  let containerItems = '';
+  array.forEach((item) => {
+    containerItems += `
       <div class="task">
       <div class="wrapper">
           <input type="checkbox" name="" id="boxes" class="box m-input" data-id=${item.index} ${item.completed ? 'checked' : ''}>
@@ -21,91 +20,80 @@ import { each } from 'lodash';
           
       </div>
       <hr>`;
-    
-    });
-  
-    container.innerHTML += containerItems;
-  
-    const boxes = document.querySelectorAll('.box');
-  
-    boxes.forEach((box) => {
-      box.addEventListener('change', () => {
-        // setCompleted(todos, box);
-        const inputtext = document.getElementById(box.dataset.id);
-        
-       const founditem = listArr.find(item => {
-         return box.dataset.id === item.index.toString()
-
-        
-        }) 
-        founditem.completed = box.checked;
-        if (founditem.completed === true) {
-          inputtext.style.textDecoration = 'line-through'; 
-        } else {
-          inputtext.style.textDecoration = 'none'; 
-        }
-        localStorage.setItem('todos', JSON.stringify(listArr));
-
-      });
-    });
-  };
-
-  
-  addBtn.addEventListener('click', () => {
-        createlocalstorage(listArr);
-        window.location.reload();
-       
-     });
-     display(listArr);
-
-    const task = document.querySelectorAll('.task');
-
-    function deleteTask(idx) {
-    const storage = JSON.parse(localStorage.getItem("todos"));
-    const savedata = storage.filter((value, index) => index !== idx);
-    localStorage.setItem('todos', JSON.stringify(savedata));
-    window.location.reload();
-  };
- 
-  task.forEach((element, index) => {
-    const el = element.childNodes[3];
-    const deletebtn = element.childNodes[5];
-    el.addEventListener('click',() =>{
-      deletebtn.style.display = 'block';
-      el.style.display = 'none';
-     element.style.background = 'rgb(214,213,164)';
-    })
-    deletebtn.addEventListener('click',()=>{
-      deleteTask(index);
-    })  
   });
 
-  function deletecompleted() {
-    deleteallbtn.addEventListener('click', () => {
-      let filteredarray = listArr.filter((object) =>  object.completed !== true);
-      filteredarray.forEach((obj, index) => {
-        obj.index = index + 1;
-      })
-      localStorage.setItem('todos', JSON.stringify(filteredarray));
-      window.location.reload();
-    })  
-  }
-  deletecompleted();
+  container.innerHTML += containerItems;
+
+  const boxes = document.querySelectorAll('.box');
+
+  boxes.forEach((box) => {
+    box.addEventListener('change', () => {
+      const inputtext = document.getElementById(box.dataset.id);
+
+      const founditem = listArr.find((item) => box.dataset.id === item.index.toString());
+      founditem.completed = box.checked;
+      if (founditem.completed === true) {
+        inputtext.style.textDecoration = 'line-through';
+      } else {
+        inputtext.style.textDecoration = 'none';
+      }
+      localStorage.setItem('todos', JSON.stringify(listArr));
+    });
+  });
+};
+
+addBtn.addEventListener('click', () => {
+  createlocalstorage(listArr);
+  window.location.reload();
+});
+display(listArr);
+
+const task = document.querySelectorAll('.task');
+
+function deleteTask(idx) {
+  const storage = JSON.parse(localStorage.getItem('todos'));
+  const savedata = storage.filter((value, index) => index !== idx);
+  localStorage.setItem('todos', JSON.stringify(savedata));
+  window.location.reload();
+}
+
+task.forEach((element, index) => {
+  const el = element.childNodes[3];
+  const deletebtn = element.childNodes[5];
+  el.addEventListener('click', () => {
+    deletebtn.style.display = 'block';
+    el.style.display = 'none';
+    element.style.background = 'rgb(214,213,164)';
+  });
+  deletebtn.addEventListener('click', () => {
+    deleteTask(index);
+  });
+});
+
+function deletecompleted() {
+  deleteallbtn.addEventListener('click', () => {
+    const filteredarray = listArr.filter((object) => object.completed !== true);
+    filteredarray.forEach((obj, index) => {
+      obj.index = index + 1;
+    });
+    localStorage.setItem('todos', JSON.stringify(filteredarray));
+    window.location.reload();
+  });
+}
+deletecompleted();
 
 function edit() {
   const inputs = document.querySelectorAll('.input-text');
   inputs.forEach((input, inputindex) => {
-    input.addEventListener('change', (e) =>{
-        listArr.filter((obj, objIndex) => {
-          if (inputindex === objIndex) {
-            obj.description = input.value;
-          }
-          return false;
-        });
+    input.addEventListener('change', () => {
+      listArr.filter((obj, objIndex) => {
+        if (inputindex === objIndex) {
+          obj.description = input.value;
+        }
+        return false;
+      });
       localStorage.setItem('todos', JSON.stringify(listArr));
-   })
-  })
+    });
+  });
 }
 edit();
- 
-
--- src/script.js
+++ src/script.js
@@ -1,19 +1,13 @@
-
 export const inputBox = document.querySelector('#add-bar');
 export const addBtn = document.querySelector('.enter');
 
-
-export let listArr = JSON.parse(localStorage.getItem('todos')) || [];
+export const listArr = JSON.parse(localStorage.getItem('todos')) || [];
 export const createlocalstorage = (array) => {
-    const localStorageObject = {
-      description: inputBox.value,
-      completed: false,
-      index: array.length + 1,
-    };
-     array.push(localStorageObject);
-     localStorage.setItem('todos', JSON.stringify(array));
-   }
-
-
-
-
+  const localStorageObject = {
+    description: inputBox.value,
+    completed: false,
+    index: array.length + 1,
+  };
+  array.push(localStorageObject);
+  localStorage.setItem('todos', JSON.stringify(array));
+};
