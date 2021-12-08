2f1259fcd6c0d81480bdce92b8264768282f93fa
Zemola <azeemolatunji@gmail.com>
2021-12-08 13:32:17 +0100

    Added jest file

---
+++ src/add-delete.js
@@ -0,0 +1,16 @@
+addBtn.addEventListener("click", () => {
+  createlocalstorage(listArr);
+  window.location.reload();
+});
+display(listArr);
+
+
+function deleteTask(idx) {
+  const storage = JSON.parse(localStorage.getItem("todos"));
+  const savedata = storage.filter((value, index) => index !== idx);
+  savedata.forEach((obj, index) => {
+    obj.index = index + 1;
+  });
+  localStorage.setItem("todos", JSON.stringify(savedata));
+  window.location.reload();
+}
