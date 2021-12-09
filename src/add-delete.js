db1f717f5fc81db18bf7de4fdf33adced7b0685d
Zemola <azeemolatunji@gmail.com>
2021-12-09 13:24:51 +0100

    Add files to Mock, included delete text function as requested

--- src/add-delete.js
+++
@@ -1,7 +0,0 @@
-function addTodoToList(description, completed = false, id, list) {
-  const todo = { description, completed, id };
-  list.push(todo);
-  return list;
-}
-
-module.exports = { addTodoToList };
