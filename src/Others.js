34e8d1e300cc53dda64595160050539d881b8320
Zemola <azeemolatunji@gmail.com>
2021-12-09 18:15:51 +0100

    Added testing function for Complete all

---
+++ src/Others.js
@@ -0,0 +1,42 @@
+export function getFromStorage() {
+  return localStorage.getItem("Tasks")
+    ? JSON.parse(localStorage.getItem("Tasks"))
+    : [];
+}
+
+export function saveToStorage(list) {
+  localStorage.setItem("Tasks", JSON.stringify(list));
+  return list;
+}
+
+function checkCompleted(list, task, element, bool) {
+  list = [];
+  list.forEach((item) => {
+    if (item.description === task) {
+      item.completed = bool; // local
+    }
+    if (bool) {
+      element.nextElementSibling.classList.add("linethrough");
+    } else {
+      element.nextElementSibling.classList.remove("linethrough");
+    }
+  });
+  return list;
+}
+
+
+export function completed(element, list) {
+  element.addEventListener("change", () => {
+    const task = element.nextElementSibling.innerHTML;
+    if (element.checked === true) {
+      checkCompleted(list, task, element, true);
+    } else {
+      checkCompleted(list, task, element, false);
+    }
+    saveToStorage(list);
+  });
+}
+
+export default { completed, getFromStorage, saveToStorage };
+
+module.exports = { checkCompleted };
